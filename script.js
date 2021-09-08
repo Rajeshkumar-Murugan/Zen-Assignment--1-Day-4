c14a1a75c1ec26e2c60e9e1d25819a8c3bf98bfd
Rajeshkumar-Murugan <85731833+Rajeshkumar-Murugan@users.noreply.github.com>
2021-09-08 14:19:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+
+function compare(obj1, obj2)
+{
+    const key1 = Object.keys(obj1);
+    const key2 = Object.keys(obj2);
+// Comparing the object with length
+    if(key1.length!==key2.length)
+    {
+        return false
+    }
+    else
+    {
+// Comparing the object with key values
+        for(let k of key1){
+            if(obj1[k]!==obj2[k])
+                {
+                    return false;
+                }
+        }
+    }
+   return true; 
+}
+
+console.log(obj1);
+console.log(obj2);
+console.log("Comparison result:" +compare(obj1, obj2));
